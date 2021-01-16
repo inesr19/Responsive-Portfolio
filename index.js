7ad2f2a558231abc0628183e1b69b8b54be9b84a
Ines Radic <inesradic12314@gmail.com>
2021-01-15 22:43:10 -0800

    update responsive html

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+  // Expands displayed gif.
+  $(document).ready(function(elem){
+    var instance = M.Materialbox.getInstance(elem);
+    $('.materialboxed').materialbox(instance);
+});
